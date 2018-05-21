d8ad47e38a10fea67f638ccc6e7c73056da6ecdb
Travis Plunk <travis.plunk@microsoft.com>
2018-05-21 12:47:31 -0700

    add logging

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -6,8 +6,10 @@ param(
   $Isolation = 'hyperv'
 )
 # Exclude Azure VM Agent folders from virus scan (
-Set-MpPreference -ExclusionPath 'C:\Packages\', 'C:\WindowsAzure\'
-Get-PackageProvider -Name nuget -ForceBootstrap -Force
+Write-Verbose -message 'Setting up defender preferences...' -verbose
+$null = Set-MpPreference -ExclusionPath 'C:\Packages\', 'C:\WindowsAzure\'
+Write-Verbose -message 'Bootstrapping nuget...' -verbose
+$null = Get-PackageProvider -Name nuget -ForceBootstrap -Force
 
 # Install docker and set machine variable that indicates we have installed it.
 # note: setting the machine variable does not update the enviroment of the current process.
@@ -16,18 +18,22 @@ Get-PackageProvider -Name nuget -ForceBootstrap -Force
 #[System.Environment]::SetEnvironmentVariable('DOCKER',"Staged",'Machine')
 
 # Set the machine to use microsoft Update and install security updates
-Get-PackageSource | Set-PackageSource -Trusted -ErrorAction SilentlyContinue
+Write-Verbose -message 'Installing modules...' -verbose
 Install-module -Name xWindowsUpdate -Confirm:$false
 Install-Module -Name xPSDesiredStateConfiguration  -Confirm:$false
 Install-Module -Name xStorage -Confirm:$false
 Install-Module -Name PSDscResources -Confirm:$false
 Install-Module -Name DockerMsftProvider  -Confirm:$false
 Install-Module -Name PackageManagementProviderResource
-Get-PackageSource | Set-PackageSource -Trusted -ErrorAction SilentlyContinue
+
+Write-Verbose -message 'Setting package sources as trusted...' -verbose
+$null = Get-PackageSource | Set-PackageSource -Trusted -ErrorAction SilentlyContinue
 
 # version 17.06.2-ee-7-tp2 was broken
+Write-Verbose -message 'Installing docker...' -verbose
 find-package -ProviderName DockerMsftProvider -MinimumVersion 17.06.1-ee  -AllVersions | ? {$_.Version -ne '17.06.2-ee-7-tp2'} | Sort-Object -Property Version -Descending | Select-Object -First 1 | Install-Package -force
 
+Write-Verbose -message 'Configuring LCM...' -verbose
 [DscLocalConfigurationManager()]
 configuration LCM {
   Settings {
@@ -40,8 +46,7 @@ configuration LCM {
 LCM
 Set-DscLocalConfigurationManager -Path .\LCM -Verbose
 
-
-
+Write-Verbose -message 'Downloading and running Configuration...' -verbose
 # run Install-ContainerHost.ps1
 $installContainerHostPath = Join-Path -Path $PSScriptRoot -ChildPath 'Install-ContainerHost.ps1'
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/TravisEz13/VstsDockerAgentBootstrap/master/install-containerhost.ps1" -OutFile $installContainerHostPath
